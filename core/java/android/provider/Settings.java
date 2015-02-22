0b642bd31e90299c5c1d71adcffe425ac4d9587a
apr20th311 <apr20th311@gmail.com>
2015-02-22 19:24:07 +0800

    a line that show the battery info (2/2)
    Change-Id: Id743beeb4305040e1c39c19da25f6ed1e6a245d0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3259,6 +3259,32 @@ public final class Settings {
         public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+       public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Whether to include audio when recording a video
          * @hide
          */
