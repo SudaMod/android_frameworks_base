0f45b8c1b7b602020ccfeb7393374dc510ea4634
ghbhaha <ghbhaha@gmail.com>
2015-03-02 22:01:06 +0800

    code format
    Change-Id: Icb15beedae62f03d7b2c4a23c8a6d39dfe72f1f6

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -528,8 +528,8 @@ class GlobalScreenshot {
             return;
         }
 
-            Bitmap ss = Bitmap.createBitmap(mDisplayMetrics.widthPixels,
-                mDisplayMetrics.heightPixels, Bitmap.Config.ARGB_8888);
+        Bitmap ss = Bitmap.createBitmap(mDisplayMetrics.widthPixels,
+        mDisplayMetrics.heightPixels, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(ss);
 
         if (requiresRotation) {
@@ -545,6 +545,7 @@ class GlobalScreenshot {
         // Recycle the previous bitmap
         mScreenBitmap.recycle();
         mScreenBitmap = ss;
+
         // Optimizations
         mScreenBitmap.setHasAlpha(false);
         mScreenBitmap.prepareToDraw();
@@ -593,9 +594,9 @@ class GlobalScreenshot {
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
                 if (Settings.System.getInt(mContext.getContentResolver(),
-            			Settings.System.SCREENSHOT_SOUND, 0) == 1)
-            		mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
-
+                    Settings.System.SCREENSHOT_SOUND, 0) == 1) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotView.buildLayer();
                 mScreenshotAnimation.start();
