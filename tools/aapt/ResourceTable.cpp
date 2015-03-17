8f1f24268aa2288accd5bc79fe46cd674fef2099
wangqianchuang <wangqianchuang@smartisan.com>
2015-03-17 15:17:47 +0800

    framework/smartisanos: add smartisanos jar.
    rootcause: add smartisanos jar.
    Signed-off-by: zhangji <zhangji@smartisan.com>

--- tools/aapt/ResourceTable.cpp
+++ tools/aapt/ResourceTable.cpp
@@ -1,5 +1,6 @@
 //
 // Copyright 2006 The Android Open Source Project
+// This code has been modified.  Portions copyright (C) 2010, T-Mobile USA, Inc.
 //
 // Build resource files from raw assets.
 //
@@ -2846,7 +2847,7 @@ status_t ResourceTable::flatten(Bundle* bundle, const sp<const ResourceFilter>& 
     for (size_t i = 0; i < basePackageCount; i++) {
         size_t packageId = table.getBasePackageId(i);
         String16 packageName(table.getBasePackageName(i));
-        if (packageId > 0x01 && packageId != 0x7f && packageId != 0x3f &&
+        if (packageId >= Res_FIRSTLIBPACKAGEID && packageId != 0x7f && packageId != 0x3f &&
                 packageName != String16("android")
                 && packageName != String16("cyanogenmod.platform")) {
             libraryPackages.add(sp<Package>(new Package(packageName, packageId)));
