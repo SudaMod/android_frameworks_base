d087a3ff2e7c9a6a03bd2dd9197ebd230d03c4e2
ghbhaha <ghbhaha@gmail.com>
2015-04-14 12:33:34 +0800

    SystemUI: fix doze pickup for shamu
    Change-Id: Id3ada2c7fe9f5993ed7773c417976021a46b7cb8

--- packages/SystemUI/src/com/android/systemui/doze/DozeService.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -123,15 +123,16 @@ public class DozeService extends DreamService implements ProximitySensorManager.
         if (mHost == null) Log.w(TAG, "No doze service host found.");
 
         setWindowless(true);
-        mDozeParameters = new DozeParameters(mContext);
+
         mSensors = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSigMotionSensor = new TriggerSensor(Sensor.TYPE_SIGNIFICANT_MOTION,
+                mDozeParameters.getPulseOnSigMotion(), mDozeParameters.getVibrateOnSigMotion(),
+                DozeLog.PULSE_REASON_SENSOR_SIGMOTION);
+        mPickupSensor = new TriggerSensor(Sensor.TYPE_PICK_UP_GESTURE,
+                mDozeParameters.getPulseOnPickup(), mDozeParameters.getVibrateOnPickup(),
+                DozeLog.PULSE_REASON_SENSOR_PICKUP);
         mUseAccelerometer = mDozeParameters.setUsingAccelerometerAsSensorPickUp();
-        if (!mUseAccelerometer) {
-            mSigMotionSensor = new TriggerSensor(Sensor.TYPE_SIGNIFICANT_MOTION,
-                mDozeParameters.getPulseOnSigMotion(), mDozeParameters.getVibrateOnSigMotion());
-            mPickupSensor = new TriggerSensor(Sensor.TYPE_PICK_UP_GESTURE,
-                mDozeParameters.getPulseOnPickup(), mDozeParameters.getVibrateOnPickup());
-        } else {
+        if (mUseAccelerometer) {
             mProximitySensorManager = new ProximitySensorManager(mContext, this);
             mShakeSensorManager = new ShakeSensorManager(mContext, this);
         }
