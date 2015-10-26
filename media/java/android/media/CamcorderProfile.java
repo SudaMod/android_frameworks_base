6fb6a564ce3dedc4359bf2d423061981cf9e537f
ghbhaha <ghbhaha@gmail.com>
2015-10-26 17:29:15 +0800

    Revert "Merge branch 'cm-12.1' into sm-1.1"
    This reverts commit 05a7a5126deb26b7f163a1d756d68ec421c74cb0.

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -125,14 +125,9 @@ public class CamcorderProfile
      */
     public static final int QUALITY_HVGA = 19;
 
-    /** @hide
-     * Quality level corresponding to 1440p resolution
-     */
-    public static final int QUALITY_1440P = 20;
-
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
-    private static final int QUALITY_LIST_END = QUALITY_1440P;
+    private static final int QUALITY_LIST_END = QUALITY_HVGA;
 
     /**
      * Time lapse quality level corresponding to the lowest available resolution.
@@ -204,14 +199,9 @@ public class CamcorderProfile
      */
     public static final int QUALITY_TIME_LAPSE_4kDCI = 1014;
 
-    /** @hide
-     * Time lapse quality level corresponding to the 1440p resolution.
-     */
-    public static final int QUALITY_TIME_LAPSE_1440P = 1015;
-
     // Start and end of timelapse quality list
     private static final int QUALITY_TIME_LAPSE_LIST_START = QUALITY_TIME_LAPSE_LOW;
-    private static final int QUALITY_TIME_LAPSE_LIST_END = QUALITY_TIME_LAPSE_1440P;
+    private static final int QUALITY_TIME_LAPSE_LIST_END = QUALITY_TIME_LAPSE_4kDCI;
 
     /**
      * High speed ( >= 100fps) quality level corresponding to the lowest available resolution.
