47930027cceac54d36a7f6746da0d470c6f1a94e
ghbhaha <ghbhaha@gmail.com>
2015-12-28 15:35:36 +0800

    Revert "add lock_flow,thanks to karon"
    This reverts commit 0de41ce2a32f27c4451d4d32668004d6d6d8554a.
    
    Conflicts:
    	packages/SystemUI/res/values-zh-rCN/sm_strings.xml
    	packages/SystemUI/res/values/sm_strings.xml
    	packages/SystemUIExt/src/com/android/systemui/statusbar/widget/LockFlow.java
    
    Change-Id: If546e2f3d1d61534d56c69b080a765bb7133d22d

--- packages/SystemUIExt/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUIExt/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -295,7 +295,7 @@ public class NetworkTraffic extends TextView {
         mState = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_NETWORK_TRAFFIC_STYLE, 3 , UserHandle.USER_CURRENT);
         mEnable = Settings.System.getIntForUser(resolver,
-                Settings.System.ENABLE_TRAFFIC_NOTIFCATION, 1 , UserHandle.USER_CURRENT) == 1;
+                Settings.System.ENABLE_TRAFFIC_NOTIFCATION, 0 , UserHandle.USER_CURRENT) == 1;
         MB = KB * KB;
         GB = MB * KB;
 
