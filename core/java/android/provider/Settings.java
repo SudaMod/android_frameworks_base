76f9aee57fee9abb9edddac756cbe19f91d57d7b
ghbhaha <ghbhaha@gmail.com>
2016-02-04 14:56:29 +0800

    fix
    Change-Id: I61cb01ae4c88354d9e50b2ade771b7019ca2d760

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2415,7 +2415,7 @@ public final class Settings {
 
 
         /**
-         * Chinese date view
+         * CLOCK_USE_SECOND
          * @hide
          */
         public static final String CLOCK_USE_SECOND = "clock_use_second";
