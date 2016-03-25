17d94653e9fc77819a0ac9a48fa29a25f059e28a
Haibo Gu <ghbhaha@users.noreply.github.com>
2016-03-25 15:46:13 +0800

    Update PhoneWindowManager.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1541,6 +1541,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case KEY_ACTION_LAST_APP:
                 ActionUtils.switchToLastApp(mContext, mCurrentUserId);
+                break;
             case KEY_ACTION_SCREENSHOT:
                 mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
                 break;
