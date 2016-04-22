6e9159deac76a3107205a060d678506560734618
Varun Date <date.varun123@gmail.com>
2016-04-22 09:51:43 +0530

    Keyguard Charging Current Switch [1/2]
    Based on this Patch
    
    https://github.com/AICP/frameworks_base/commit/a86888febb62520de117ad6a01740d651ac96044
    
    Thanks to yank555-lu for the initial patch
    
    Just adding an option to turn it on/off
    
    Change-Id: Ida6a887dad10530b1ac37d80f4458ee1c085ad28
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3436,6 +3436,12 @@ public final class Settings {
         public static final Validator SIP_ASK_ME_EACH_TIME_VALIDATOR = sBooleanValidator;
 
         /**
+         * whether to enable /disable showing charging current on lockscreen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_CURRENT = "lock_screen_show_current";
+
+        /**
          * Pointer speed setting.
          * This is an integer value in a range between -7 and +7, so there are 15 possible values.
          *   -7 = slowest
