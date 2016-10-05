29f2cfb6b026f79b890a053c7c2b6a634febeadb
sdhz150 <yanghui140@gmail.com>
2016-10-05 10:23:54 +0800

    Status bar Carriers
    Change-Id: I27c4dd8141874e93340b5c383c8d41f1fb74c6fb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -47,6 +47,7 @@ import com.android.systemui.statusbar.SignalClusterView;
 import com.android.systemui.statusbar.StatusBarIconView;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
+import com.android.systemui.statusbar.widget.CarrierLabel;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -78,6 +79,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
     private BatteryMeterView mBatteryMeterViewKeyguard;
     private ClockController mClockController;
     private View mCenterClockLayout;
+    private CarrierLabel mCarrierLabel;
 
     private int mIconSize;
     private int mIconHPadding;
@@ -136,6 +138,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mDarkModeIconColorSingleTone = context.getColor(R.color.dark_mode_icon_color_single_tone);
         mLightModeIconColorSingleTone = context.getColor(R.color.light_mode_icon_color_single_tone);
         mHandler = new Handler();
+        mCarrierLabel = (CarrierLabel) statusBar.findViewById(R.id.carrier_label);
         mClockController = new ClockController(statusBar, mNotificationIconAreaController, mHandler);
         mCenterClockLayout = statusBar.findViewById(R.id.center_clock_layout);
         defineSlots();
@@ -540,6 +543,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mBatteryMeterView.setDarkIntensity(
                 isInArea(mTintArea, mBatteryMeterView) ? mDarkIntensity : 0);
         mClockController.setTextColor(mTintArea, mIconTint);
+        mCarrierLabel.setTextColor(mIconTint);
     }
 
     public void appTransitionPending() {
