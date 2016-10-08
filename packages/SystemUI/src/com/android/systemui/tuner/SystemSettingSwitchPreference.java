0c5cdc4b4b0a19aea470a11d9063c3ab0cfc6387
ghbhaha <ghbhaha@gmail.com>
2016-10-08 08:57:11 +0800

    Revert "fix compile"
    This reverts commit e3a3bb432bf64693881b800cdeae96cdeac6a711.

--- packages/SystemUI/src/com/android/systemui/tuner/SystemSettingSwitchPreference.java
+++ packages/SystemUI/src/com/android/systemui/tuner/SystemSettingSwitchPreference.java
@@ -56,4 +56,10 @@ public class SystemSettingSwitchPreference extends SwitchPreference {
                 getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
 
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
 }
