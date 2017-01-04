b9e7d17836af4d76f3e1fc97520234691cbb0b74
beanstown106 <nbenis106@gmail.com>
2017-01-04 13:29:17 -0500

    Pixel Navbar animation toggle [1/2] *made to work with the exposure for themers to disable navbar animation
    Change-Id: If15aff30447a65b9199d1768802ce86f1e1e1615

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3881,6 +3881,12 @@ public final class Settings {
          */
         public static final String DISABLE_STK_DIALOG_AUTO_OPEN = "disable_stk_dialog_auto_open";
 
+         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
