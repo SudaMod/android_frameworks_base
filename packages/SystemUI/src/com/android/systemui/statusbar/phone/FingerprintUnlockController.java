6187a95a307a31accce9946f7a1fd6e90c1e61f2
Sultanxda <sultanxda@gmail.com>
2017-05-29 23:50:53 +0530

    Fingerprint: Speed up wake-and-unlock scenario
    *Directly dismiss keyguard instead of using a fade-out animation
    *Skip virtual navigation button animation on wake
    
    Change-Id: I0f1e4503a92131bdc59a88cc09c2bbf574a3ff48
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/FingerprintUnlockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/FingerprintUnlockController.java
@@ -264,6 +264,7 @@ public class FingerprintUnlockController extends KeyguardUpdateMonitorCallback {
             case MODE_NONE:
                 break;
         }
+        mStatusBarWindowManager.setForceDozeBrightness(false);
         mStatusBar.notifyFpAuthModeChanged();
         Trace.endSection();
     }
