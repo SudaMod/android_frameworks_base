ba312bd443750311ab09067edf9a86bef662a483
sdhz150 <yanghui140@gmail.com>
2020-02-09 12:07:38 +0800

    UpdatConstant

--- suda/java/android/suda/lunar/Constant.java
+++ suda/java/android/suda/lunar/Constant.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The SudaMod Project 
+ * Copyright (C) 2015 The SudaMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,23 +44,23 @@ public class Constant {
 
     public static final String[] ANIMALS_CN = {"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪"};
     public static final String[] ANIMALS_HK_TW = {"鼠", "牛", "虎", "兔", "龍", "蛇", "馬", "羊", "猴", "雞", "狗", "豬"};
- 
+
     public static final String[] LUNAR_FESTIVAL_CN = {"腊月廿九 除夕", "腊月三十 除夕", "正月初一 春节", "正月十五 元宵节", "五月初五 端午节", "七月初七 七夕节", "七月十五 中元节", "八月十五 中秋节", "九月初九 重阳节", "腊月初八 腊八节", "腊月廿三 小年"};
     public static final String[] LUNAR_FESTIVAL_HK_TW = {"臘月廿九 除夕", "臘月三十 除夕", "正月初一 春節", "正月十五 元宵節", "五月初五 端午節", "七月初七 七夕節", "七月十五 中元節", "八月十五 中秋節", "九月初九 重陽節", "腊月初八 臘八節", "臘月廿三 小年"};
 
     public static final String[] HOLIDAY_CN = {"0101 元旦", "0214 情人节", "0308 妇女节", "0312 植树节",
                     "0315 消费日", "0401 愚人节", "0413 泼水节", "0501 劳动节", "0504 青年节", "0601 儿童节",
                     "0701 建党日", "0801 建军节", "0903 抗战胜利", "0910 教师节", "1001 国庆节", "1031 万圣节",
-                    "1111 光棍节", "1224 平安夜", "1225 圣诞节"};
+                    "1111 光棍节", "1213 国家公祭日", "1224 平安夜", "1225 圣诞节"};
 
     public static final String[] HOLIDAY_HK = {"0101 元旦", "0214 情人節", "0308 婦女節", "0401 愚人節",
-                    "0501 勞動節", "0701 特區紀念", "0910 教師節", "1001 國慶節", "1031 萬聖節", "1224 平安夜",
+                    "0501 勞動節", "0701 特區紀念", "0910 教師節", "1001 國慶節", "1031 萬聖節", "1213 國家公祭日", "1224 平安夜",
                     "1225 聖誕節"};
 
     public static final String[] HOLIDAY_TW = {"0101 元旦", "0214 情人節", "0228 和平紀念", "0308 婦女節",
                     "0312 國父逝世", "0314 反侵略日", "0329 先烈紀念", "0401 愚人節", "0404 兒童節", "0501 勞動節",
                     "0715 解放紀念", "0808 父親節", "0903 軍人節", "0928 孔子誕辰", "1010 國慶節", "1024 聯合國日", "1025 臺灣光復",
-                    "1112 國父誕辰", "1031 萬聖節", "1224 平安夜", "1225 聖誕節"};
+                    "1031 萬聖節", "1112 國父誕辰", "1213 國家公祭日", "1224 平安夜", "1225 聖誕節"};
 
     public static final String[] SPEC_DAY_CN = {"5,2,0,母亲节", "6,3,0,父亲节", "11,4,4,感恩节"};
     public static final String[] SPEC_DAY_HK = {"5,2,0,母親節", "6,3,0,父親節", "11,4,4,感恩節"};
@@ -85,11 +85,11 @@ public class Constant {
     public static String[] getSolarHoliday() {
         Locale locale = Resources.getSystem().getConfiguration().locale;
         if (locale.getCountry().equals("CN")) {
-            return HOLIDAY_CN;		
+            return HOLIDAY_CN;
         } else if (locale.getCountry().equals("HK")) {
             return HOLIDAY_HK;
         } else if (locale.getCountry().equals("TW")) {
-            return HOLIDAY_TW;	
+            return HOLIDAY_TW;
         }
         return HOLIDAY_CN;
     }
@@ -97,11 +97,11 @@ public class Constant {
     public static String[] getSpecday() {
         Locale locale = Resources.getSystem().getConfiguration().locale;
         if (locale.getCountry().equals("CN")) {
-            return SPEC_DAY_CN;		
+            return SPEC_DAY_CN;
         } else if (locale.getCountry().equals("HK")) {
             return SPEC_DAY_HK;
         } else if (locale.getCountry().equals("TW")) {
-            return SPEC_DAY_TW;	
+            return SPEC_DAY_TW;
         }
         return SPEC_DAY_CN;
     }
